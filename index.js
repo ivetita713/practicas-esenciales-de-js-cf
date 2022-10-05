9197eaf16e7166e22f6c8eac32e8485d7e1d5214
Iveth <england_713@hotmail.com>
2022-10-05 05:23:37 +0000

    Update index.js and package-lock.json

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import "./src/1-functions";
-// import "./src/2-scope";
+//import "./src/1-functions";
+import "./src/2-scope";
 // import "./src/3-block-scope";
 // import "./src/4-hoisting";
 // import "./src/5-1-context";
